f7f90a0fd7ce8bfdc79cbd16ee567260dbd83995
Evan <alfeushendrievan@gmail.com>
2026-04-15 01:49:14 +0700

    buat auth services

---
+++ lib/data/services/auth_services.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+
+  Future<User?> signUp(String email, String password) async {
+    final result = await _auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    return result.user;
+  }
+
+  
+  Future<User?> signIn(String email, String password) async {
+    final result = await _auth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    return result.user;
+  }
+
+  
