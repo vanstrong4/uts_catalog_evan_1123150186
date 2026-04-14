1af3912d59a6d5cdf9b261fc1bc3b7e72d8456da
Evan <alfeushendrievan@gmail.com>
2026-04-15 01:49:38 +0700

    buat auth services kode sign in with google

--- lib/data/services/auth_services.dart
+++ lib/data/services/auth_services.dart
@@ -4,7 +4,6 @@ import 'package:google_sign_in/google_sign_in.dart';
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-
   Future<User?> signUp(String email, String password) async {
     final result = await _auth.createUserWithEmailAndPassword(
       email: email,
@@ -13,7 +12,6 @@ class AuthService {
     return result.user;
   }
 
-  
   Future<User?> signIn(String email, String password) async {
     final result = await _auth.signInWithEmailAndPassword(
       email: email,
@@ -22,4 +20,23 @@ class AuthService {
     return result.user;
   }
 
-  
+  Future<User?> signInWithGoogle() async {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    if (googleUser == null) return null;
+
+    final googleAuth = await googleUser.authentication;
+
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    final result = await _auth.signInWithCredential(credential);
+    return result.user;
+  }
+
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+}
