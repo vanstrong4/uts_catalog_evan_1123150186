c790edb0d8bd7d76c5ad5a064e20a3204fe44c0c
Evan <alfeushendrievan@gmail.com>
2026-04-17 06:45:06 +0700

    bikin reusable appbar

---
+++ lib/features/auth/widgets/custom_appbar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+
+  const CustomAppBar({required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(title: Text(title), backgroundColor: Colors.lightBlue);
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+}
