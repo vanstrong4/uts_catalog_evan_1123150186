f01669f4f6dd0e1ac51f66aa89535281588147f4
Evan <alfeushendrievan@gmail.com>
2026-04-17 06:33:53 +0700

    bikin reusable button

---
+++ lib/features/auth/widgets/custom_buttons.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+  const CustomButton({required this.text, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.lightBlue,
+          padding: EdgeInsets.symmetric(vertical: 14),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        ),
+        onPressed: onPressed,
+        child: Text(text, style: TextStyle(color: Colors.white)),
+      ),
+    );
+  }
+}
