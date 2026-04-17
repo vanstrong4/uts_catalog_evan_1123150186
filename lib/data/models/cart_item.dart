263f1b73876ecc76df1690d88f033d508cb2c9b7
Evan <alfeushendrievan@gmail.com>
2026-04-17 22:34:45 +0700

    bikin cart_item

---
+++ lib/data/models/cart_item.dart
@@ -0,0 +1,13 @@
+class CartItem {
+  final String name;
+  final int price;
+  final String image;
+  final String description;
+
+  CartItem({
+    required this.name,
+    required this.price,
+    required this.image,
+    required this.description,
+  });
+}
