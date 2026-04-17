f57c6be22c81cb5a2d7ae391786bc5f3e7c1fc66
Evan <alfeushendrievan@gmail.com>
2026-04-17 07:33:56 +0700

    revisi login jadi make custom reusable

--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import '../../../data/services/auth_service.dart';
 import '../../catalog/screens/home_screen.dart';
-import '../../../widgets/custom_textfield.dart';
-import '../../../widgets/custom_button.dart';
+import '../../auth/widgets/custom_textfield.dart';
+import '../../auth/widgets/custom_buttons.dart';
 import 'register_screen.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -33,137 +33,52 @@ class LoginScreen extends StatelessWidget {
                       children: [
                         SizedBox(height: 40),
 
+                   
+                            if (!email.contains("@")) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                  content: Text("Format email tidak valid"),
+                                ),
+                              );
+                              return;
+                            }
+
+                            try {
+                              await auth.signIn(email, password);
+
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(builder: (_) => HomeScreen()),
+                              );
+
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(content: Text("Login berhasil")),
+                              );
+                            } catch (e) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                  content: Text("Email atau password salah"),
+                                ),
+                              );
+                            }
+                          },
+                        ),
+
+                        SizedBox(height: 10),
+
+                        // GOOGLE LOGIN
                         Center(
-                          child: Image.asset("images/ecs.png", height: 180),
-                        ),
-
-                        SizedBox(height: 20),
-
-                        Text(
-                          "Login",
-                          style: TextStyle(
-                            fontSize: 26,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.lightBlue[700],
-                          ),
-                        ),
-
-                        SizedBox(height: 8),
-
-                        Text(
-                          "Masuk ke akun kamu",
-                          style: TextStyle(color: Colors.lightBlue),
-                        ),
-
-                        SizedBox(height: 30),
-
-                        TextField(
-                          controller: emailController,
-                          keyboardType: TextInputType.emailAddress,
-                          style: TextStyle(color: Colors.lightBlue),
-                          decoration: InputDecoration(
-                            hintText: "Email",
-                            hintStyle: TextStyle(
-                              color: Colors.lightBlue.shade200,
-                            ),
-                            enabledBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.lightBlue),
-                            ),
-                            focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.lightBlue),
-                            ),
-                          ),
-                        ),
-
-                        SizedBox(height: 20),
-
-                        TextField(
-                          controller: passwordController,
-                          obscureText: true,
-                          style: TextStyle(color: Colors.lightBlue),
-                          decoration: InputDecoration(
-                            hintText: "Password",
-                            hintStyle: TextStyle(
-                              color: Colors.lightBlue.shade200,
-                            ),
-                            enabledBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.lightBlue),
-                            ),
-                            focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.lightBlue),
-                            ),
-                          ),
-                        ),
-
-                        SizedBox(height: 30),
-
-                        SizedBox(
-                          width: double.infinity,
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.lightBlue,
-                              padding: EdgeInsets.symmetric(vertical: 14),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(8),
-                              ),
-                            ),
+                          child: TextButton(
                             onPressed: () async {
-                              String email = emailController.text.trim();
-                              String password = passwordController.text.trim();
-
-                              if (email.isEmpty || password.isEmpty) {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(
-                                    content: Text(
-                                      "Email & Password wajib diisi",
-                                    ),
-                                  ),
-                                );
-                                return;
-                              }
-
-                              if (!email.contains("@")) {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(
-                                    content: Text("Format email tidak valid"),
-                                  ),
-                                );
-                                return;
-                              }
-
                               try {
-                                await auth.signIn(email, password);
+                                await auth.signInWithGoogle();
+
                                 Navigator.pushReplacement(
                                   context,
                                   MaterialPageRoute(
                                     builder: (_) => HomeScreen(),
                                   ),
                                 );
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(content: Text("Login berhasil")),
-                                );
-                              } catch (e) {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(
-                                    content: Text("Email atau password salah"),
-                                  ),
-                                );
-                              }
-                            },
-                            child: Text(
-                              "Login",
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ),
-                        ),
-
-                        SizedBox(height: 10),
-
-                        Center(
-                          child: TextButton(
-                            onPressed: () async {
-                              try {
-                                await auth.signInWithGoogle();
                               } catch (e) {
                                 ScaffoldMessenger.of(context).showSnackBar(
                                   SnackBar(content: Text("Login Google gagal")),
@@ -179,6 +94,7 @@ class LoginScreen extends StatelessWidget {
 
                         SizedBox(height: 20),
 
+                        // REGIS
                         Center(
                           child: Row(
                             mainAxisAlignment: MainAxisAlignment.center,
