9f6bb0ca611721cd73797550e9b6d9c366c3cb54
Evan <alfeushendrievan@gmail.com>
2026-04-22 14:46:05 +0700

    benerin auth

--- lib/data/services/auth_service.dart
+++ lib/data/services/auth_service.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:crypto/crypto.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
@@ -11,70 +12,4 @@ class AuthService {
       password: password,
     );
 
-    await userCredential.user!.sendEmailVerification();
-  }
-
-  Future<String> signIn(String email, String password) async {
-    final userCredential = await _auth.signInWithEmailAndPassword(
-      email: email,
-      password: password,
-    );
-
-    final user = userCredential.user!;
-
-    if (!user.emailVerified) {
-      await _auth.signOut();
-      throw Exception("Email belum diverifikasi");
-    }
-
-    final accessToken = _generateAccessToken(user);
-
-    return accessToken;
-  }
-
-  Future<User?> signInWithGoogle() async {
-    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-
-    if (googleUser == null) return null;
-
-    final googleAuth = await googleUser.authentication;
-
-    final credential = GoogleAuthProvider.credential(
-      accessToken: googleAuth.accessToken,
-      idToken: googleAuth.idToken,
-    );
-
-    final result = await _auth.signInWithCredential(credential);
-    return result.user;
-  }
-
-  Future<void> signOut() async {
-    await _auth.signOut();
-  }
-
-  String _generateAccessToken(User user) {
-    // HEADER
-    final header = base64UrlEncode(
-      utf8.encode(jsonEncode({"alg": "HS256", "typ": "JWT"})),
-    );
-
-    final payload = base64UrlEncode(
-      utf8.encode(
-        jsonEncode({
-          "sub": user.uid,
-          "email": user.email,
-          "iss": "evan-machine-store",
-          "iat": DateTime.now().millisecondsSinceEpoch ~/ 1000,
-          "exp":
-              DateTime.now().add(Duration(hours: 1)).millisecondsSinceEpoch ~/
-              1000,
-        }),
-      ),
-    );
-
-    final signatureRaw = "$header.$payload.evansignaturekey";
-    final signature = base64UrlEncode(utf8.encode(signatureRaw));
-
-    return "$header.$payload.$signature";
-  }
-}
+    await 
