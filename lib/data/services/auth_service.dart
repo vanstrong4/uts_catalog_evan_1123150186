4896b8cf67b587125b5de573f79f99aefab50218
Evan <alfeushendrievan@gmail.com>
2026-04-22 14:24:15 +0700

    perbaikan auth service selesai

--- lib/data/services/auth_service.dart
+++ lib/data/services/auth_service.dart
@@ -5,7 +5,6 @@ import 'package:google_sign_in/google_sign_in.dart';
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  
   Future<void> signUp(String email, String password) async {
     final userCredential = await _auth.createUserWithEmailAndPassword(
       email: email,
@@ -15,9 +14,67 @@ class AuthService {
     await userCredential.user!.sendEmailVerification();
   }
 
-
   Future<String> signIn(String email, String password) async {
     final userCredential = await _auth.signInWithEmailAndPassword(
       email: email,
       password: password,
     );
+
+    final user = userCredential.user!;
+
+    if (!user.emailVerified) {
+      await _auth.signOut();
+      throw Exception("Email belum diverifikasi");
+    }
+
+    final accessToken = _generateAccessToken(user);
+
+    return accessToken;
+  }
+
+  Future<User?> signInWithGoogle() async {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    if (googleUser == null) return null;
+
+    final googleAuth = await googleUser.authentication;
+
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    final result = await _auth.signInWithCredential(credential);
+    return result.user;
+  }
+
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+
+  String _generateAccessToken(User user) {
+    // HEADER
+    final header = base64UrlEncode(
+      utf8.encode(jsonEncode({"alg": "HS256", "typ": "JWT"})),
+    );
+
+    final payload = base64UrlEncode(
+      utf8.encode(
+        jsonEncode({
+          "sub": user.uid,
+          "email": user.email,
+          "iss": "evan-machine-store",
+          "iat": DateTime.now().millisecondsSinceEpoch ~/ 1000,
+          "exp":
+              DateTime.now().add(Duration(hours: 1)).millisecondsSinceEpoch ~/
+              1000,
+        }),
+      ),
+    );
+
+    final signatureRaw = "$header.$payload.evansignaturekey";
+    final signature = base64UrlEncode(utf8.encode(signatureRaw));
+
+    return "$header.$payload.$signature";
+  }
+}
