64e69d89167fc38592f89293e42f6d590a487b99
vanstrong4 <alfeushendrievan@gmail.com>
2026-04-22 15:29:14 +0700

    update kode dengan komentar

--- lib/data/services/auth_service.dart
+++ lib/data/services/auth_service.dart
@@ -6,15 +6,17 @@ import 'package:google_sign_in/google_sign_in.dart';
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
+  // register akun baru
   Future<void> signUp(String email, String password) async {
     final userCredential = await _auth.createUserWithEmailAndPassword(
       email: email,
       password: password,
     );
-
+    // mengirimkan verifikasi email
     await userCredential.user!.sendEmailVerification();
   }
 
+  // login dan JWT
   Future<String> signIn(String email, String password) async {
     final userCredential = await _auth.signInWithEmailAndPassword(
       email: email,
@@ -22,16 +24,16 @@ class AuthService {
     );
 
     final user = userCredential.user!;
-
+    // cek dulu emailnya udah di verif belom
     if (!user.emailVerified) {
       await _auth.signOut();
       throw Exception("Email belum diverifikasi");
     }
-
     final token = _generateAccessToken(user);
     return token;
   }
 
+  // login dengan google
   Future<User?> signInWithGoogle() async {
     final googleUser = await GoogleSignIn().signIn();
     if (googleUser == null) return null;
@@ -47,6 +49,7 @@ class AuthService {
     return result.user;
   }
 
+  // logout dari firebase
   Future<void> signOut() async {
     await _auth.signOut();
   }
